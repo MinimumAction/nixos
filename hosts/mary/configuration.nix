9bd973be632e66514bcaf3cb2d8df49dfcdf7087
MinimumAction <liguori.km@gmail.com>
2025-06-30 13:43:32 -0400

    readded zfs and btrfs

--- hosts/mary/configuration.nix
+++ hosts/mary/configuration.nix
@@ -11,6 +11,10 @@
 
   # Boot parameters to support zfs & include drivers for external wifi adapter 
   boot = {
+    supportedFilesystems = [ 
+      "zfs"
+      "btrfs" 
+    ];
     extraModulePackages = with config.boot.kernelPackages; [ rtl8812au ];
     initrd.kernelModules = [ "8812au" ];
   };
