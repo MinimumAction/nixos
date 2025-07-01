20bb7154693eed719a3f44334204cab935399353
MinimumAction <liguori.km@gmail.com>
2025-07-01 17:52:10 -0400

    changed neovim config

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,9 +1,19 @@
-{ pkgs, ... }:{
- 
-  programs.neovim = {
-    enable = true;
-    package = pkgs.neovim;
-    plugins = [];
-  };
+{ config, pkgs, ... }:{
 
+  environment.systemPackages = [
+    (pkgs.neovim.override {
+      configure = {
+        packages.myPlugins = with pkgs.vimPlugins; {
+          start = [
+            # lualine-nvim
+            telescope-nvim
+            # plenary-nvim
+            # nvim-treesitter
+            # nvim-lspconfig
+            # vim-nix
+          ];
+        };
+      };
+    })
+  ];
 }
