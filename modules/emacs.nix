568a7fc2515e3ac342d8b1c5757bd7d9aeb07305
MinimumAction <liguori.km@gmail.com>
2025-07-01 19:02:30 -0400

    added some epkgs to emacs

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -2,5 +2,9 @@
   services.emacs = {
     enable = true;
     package = pkgs.emacs-gtk; # emacs-gtk for wayland support
+    extraPackages = epkgs: [ 
+      epkgs.use-package 
+      epkgs.magit 
+    ];
   };
 }
