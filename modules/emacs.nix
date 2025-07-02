0b33cee3833d7cf5667a5f3025dc1be28e80ef31
MinimumAction <liguori.km@gmail.com>
2025-07-01 21:54:14 -0400

    fixed emacs to start daemon graphically?

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,16 +1,18 @@
 { pkgs, ... }:{
 
+  environment.systemPackages = with pkgs; [
+    emacs-pgtk
+  ];
+
   systemd.user.services.emacs = {
-  description = "Emacs daemon (PGTK)";
-  after = [ "graphical-session.target" ];
-  partOf = [ "graphical-session.target" ];
-
-  serviceConfig = {
-    ExecStart = "${pkgs.emacs-pgtk}/bin/emacs --fg-daemon";
-    Restart = "on-failure";
-    Environment = "WAYLAND_DISPLAY=wayland-0";
+    description = "Emacs daemon (PGTK)";
+    after = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.emacs-pgtk}/bin/emacs --fg-daemon";
+      Restart = "on-failure";
+      Environment = "WAYLAND_DISPLAY=wayland-0";
+    };
   };
-};
-
 
 }
