11340886068bfc7dcb2e57a0403a1ec9933e970b
MinimumAction <liguori.km@gmail.com>
2025-07-01 21:47:17 -0400

    fix emacs so that the daemon is lauched in a graphical environment

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,8 +1,19 @@
 { pkgs, ... }:{
 
-  services.emacs = {
-    enable = true;
-    package = pkgs.emacs-pgtk; # Use the gtk for wayland support
+  systemd.user.services.emacs = {
+    Unit = {
+      Description = "Emacs daemon (PGTK)";
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.emacs-pgtk}/bin/emacs --fg-daemon";
+      Restart = "on-failure";
+      Environment = "WAYLAND_DISPLAY=wayland-0";
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
   };
 
 }
