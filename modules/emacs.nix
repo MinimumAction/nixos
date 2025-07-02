dabb76cef817e5b183b07248ad6a0fbb4bb9fb0d
MinimumAction <liguori.km@gmail.com>
2025-07-01 22:51:56 -0400

    back to old emacs config

--- modules/emacs.nix
+++ modules/emacs.nix
@@ -1,18 +1,8 @@
 { pkgs, ... }:{
 
-  environment.systemPackages = with pkgs; [
-    emacs-pgtk
-  ];
-
-  systemd.user.services.emacs = {
-    description = "Emacs daemon (PGTK)";
-    after = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.emacs-pgtk}/bin/emacs --fg-daemon";
-      Restart = "on-failure";
-      Environment = "WAYLAND_DISPLAY=wayland-0";
-    };
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs-pgtk; # For wayland support
   };
 
 }
