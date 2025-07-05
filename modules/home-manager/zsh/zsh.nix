54b02867f8cdca48115ebf86e33df2e8d88039f2
MinimumAction <liguori.km@gmail.com>
2025-07-04 22:04:28 -0400

    added some shell aliases

--- modules/home-manager/zsh/zsh.nix
+++ modules/home-manager/zsh/zsh.nix
@@ -7,6 +7,12 @@ in
   programs.zsh = {
     enable = true;
 
+    shellAliases = {
+      ll = "ls -lah";
+      gs = "git status";
+      rebuild = "sudo nixos-rebuild switch --flake .#$(hostname)";
+    };
+
     initContent = ''
       bindkey -v
       PROMPT='%F{${c.cyan}}%n@%m%f %F{${c.yellow}}%~%f %# '
